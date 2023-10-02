41431e5b9be2ea5e1f92ccec119da273d411ae40
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-10-02 20:27:35 -0300

    test editora

--- APITest/src/test/models/editora.test.js
+++ APITest/src/test/models/editora.test.js
@@ -15,4 +15,30 @@ describe('Testando o modelo Editora', () => {
             expect.objectContaining(objetoEditora),
         );
     })
+
+    // skip - Pular o teste
+    it.skip('Deve salvar editora no BD', () => {
+        const editora = new Editora(objetoEditora);
+
+        editora.salvar().then((dados) => {
+            expect(dados.nome).toBe('CDC');
+        })
+    })
+
+    it('Deve salvar editora no BD usando sintaxe moderna', async() => {
+        const editora = new Editora(objetoEditora);
+
+        const dados = await editora.salvar();
+
+        const retornado = await Editora.pegarPeloId(dados.id);
+
+        expect(retornado).toEqual(
+            expect.objectContaining({
+                id: expect.any(Number),
+                ...objetoEditora, 
+                created_at: expect.any(String),
+                updated_at: expect.any(String),
+            })
+        )
+    })
 })
